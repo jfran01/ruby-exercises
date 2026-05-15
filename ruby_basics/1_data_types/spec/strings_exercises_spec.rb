7869ee1fd005c3d0893df2d30ab2040a65a7a85e
Juliette <211356692+jfran01@users.noreply.github.com>
2026-05-15 22:15:39 +0100

    merge all exercises - did not commit in prior sessions

--- ruby_basics/1_data_types/spec/strings_exercises_spec.rb
+++ ruby_basics/1_data_types/spec/strings_exercises_spec.rb
@@ -16,14 +16,14 @@ RSpec.describe 'String Exercises' do
       expect(concatenate('world')).to eq('Hello world!')
     end
 
-    xit 'returns "Hello universe!"' do
+    it 'returns "Hello universe!"' do
       expect(concatenate('universe')).to eq('Hello universe!')
     end
   end
 
   describe 'substrings exercise' do
 
-    xit 'returns the first 4 letters of the word' do
+    it 'returns the first 4 letters of the word' do
       expect(substrings('chocolate')).to eq('choc')
     end
   end
