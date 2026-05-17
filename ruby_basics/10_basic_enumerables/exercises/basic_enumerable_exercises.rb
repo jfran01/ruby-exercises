4716347d7d4b737e719c64b4b61dc4b82c79c948
Juliette <211356692+jfran01@users.noreply.github.com>
2026-05-17 21:08:43 +0100

    use each_with_index to iterate through a list

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -8,6 +8,7 @@ def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index{|item, idx| puts "Guess ##{idx + 1} is #{item}"}
 end
 
 def find_absolute_values(numbers)
